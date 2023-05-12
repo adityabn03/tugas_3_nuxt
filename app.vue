3929b4d1d64dbac3f8a051c7328796bb9ac365f7
Aditya Bimantoro Nurhaqi <adityanurhaqi03@gmail.com>
2023-05-12 12:36:07 +0700

    ini nuxt ya

---
+++ app.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+  <section class="bg-gray-600 py-12">
+<h1 class="text-white text-center text-3xl font-bold tracking-wider">Resep Nasi Goreng</h1>
+</section>
+<main>
+<section class="bg-gray-100 flex justify-center items-center h-60" style="background-image: url('https://cdn.idntimes.com/content-images/community/2021/11/pexels-nadim-shaikh-7758253-imresizer-5f33a6106b7b2abdfec00ff311918826-25d0d1a3a9e31db310684a75783015bf.jpg'); background-size: cover; background-position: center;">
+  <div class="text-gray-800 text-center px-4 sm:px-10 md:px-20 lg:px-40 xl:px-60">
+    <h2 class="text-2xl font-bold mb-4 text-white">Mari Memasak Bersama Adit</h2>
+    <h2 class="text-xl font-bold mb-1 text-white">Ayo Kita Masak Masakan Yang Mantap</h2>
+  </div>
+</section>
+<section class="bg-gray-600">
+<div class="text-white px-8 sm:px-12 md:px-24 lg:px-40 xl:px-60 py-10">
+  <div class="flex flex-col md:flex-row justify-between">
+    <div class="w-full md:w-1/2 mb-8 md:mb-0">
+      <h3 class="text-2xl font-bold mb-4">Bahan-bahan:</h3>
+      <ul class="list-disc pl-6">
+        <li class="mb-2">Nasi putih (1 mangkuk)</li>
+        <li class="mb-2">Telur (2 butir, kocok lepas)</li>
+        <li class="mb-2">Bawang putih (2 siung, cincang halus)</li>
+        <li class="mb-2">Bawang merah (2 siung, cincang halus)</li>
+        <li class="mb-2">Cabe merah (2 buah, iris tipis)</li>
+        <li class="mb-2">Wortel (1 buah, iris tipis)</li>
+        <li class="mb-2">Kol (1/4 buah, iris tipis)</li>
+        <li class="mb-2">Saus tiram (1 sdm)</li>
+        <li class="mb-2">Kecap manis (2 sdm)</li>
+        <li>Garam (secukupnya)</li>
+      </ul>
+    </div>
+    <div class="w-full md:w-1/2">
+      <h3 class="text-2xl font-bold mb-4">Cara Memasak:</h3>
+      <ol class="list-decimal pl-6">
+        <li class="mb-4">Panaskan minyak goreng di wajan.</li>
+        <li class="mb-4">Tumis bawang putih, bawang merah, dan cabe merah hingga harum.</li>
+        <li class="mb-4">Tambahkan telur kocok dan aduk hingga matang.</li>
+        <li class="mb-4">Masukkan wortel dan kol, aduk rata.</li>
+        <li class="mb-4">Tambahkan nasi putih, saus tiram, kecap manis, dan garam, aduk hingga merata.</li>
+        <li class="mb-4">Masak selama 5-7 menit hingga nasi matang dan bumbu meresap.</li>
+        <li class="mb-4">Sajikan nasi goreng dan beri taburan daun bawang atau kerupuk.</li>
+      </ol>
+    </div>
+  </div>
+</div>
+</section>
+   <section class="bg-gray-100" style="background-image: url('https://cdn.idntimes.com/content-images/community/2021/11/amanda-lim-2-vyvemgd04-unsplash-imresizer-5f33a6106b7b2abdfec00ff311918826-a32e6a0184b0e1b99c076973bfbeb5cf.jpg'); background-size: cover; background-position: center;">
+      <div class="px-4 sm:px-10 md:px-20 lg:px-40 xl:px-60 py-6">
+        <h2 class="text-2xl font-bold mb-4">Video Tutorial:</h2>
+        <div>
+          <iframe class="w-full h-96" src="https://www.youtube.com/embed/znCSG8-f1_E" title="YouTube video player" allowfullscreen></iframe>
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer class="bg-gray-900 py-2 text-white text-center">
+    <p>© 2023, Tutorial Masak Nasi Goreng Adit</p>
+  </footer>
+</div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+</style>
