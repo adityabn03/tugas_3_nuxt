92d62ed8995831ebfb5d09a4f4778292ab039edc
Aditya Bimantoro Nurhaqi <adityanurhaqi03@gmail.com>
2023-05-12 13:34:45 +0700

    style: tambah menu bar

--- app.vue
+++ app.vue
@@ -1,8 +1,31 @@
 <template>
-  <div>
-  <section class="bg-gray-600 py-12">
-<h1 class="text-white text-center text-3xl font-bold tracking-wider">Resep Nasi Goreng</h1>
-</section>
+  <div class="container">
+    <nav class="bg-gray-800 py-4">
+      <div class="mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <svg class="h-8 w-8 text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 2.4a7.6 7.6 0 100 15.2 7.6 7.6 0 000-15.2zM10 0a10 10 0 100 20 10 10 0 000-20z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <nuxt-link to="/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Home</nuxt-link>
+                <nuxt-link to="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</nuxt-link>
+                <nuxt-link to="/resep" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Resep</nuxt-link>
+                <nuxt-link to="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="bg-gray-600 py-12">
+      <h1 class="text-white text-center text-3xl font-bold tracking-wider">Resep Nasi Goreng</h1>
+    </section>
+
 <main>
 <section class="bg-gray-100 flex justify-center items-center h-60" style="background-image: url('https://cdn.idntimes.com/content-images/community/2021/11/pexels-nadim-shaikh-7758253-imresizer-5f33a6106b7b2abdfec00ff311918826-25d0d1a3a9e31db310684a75783015bf.jpg'); background-size: cover; background-position: center;">
   <div class="text-gray-800 text-center px-4 sm:px-10 md:px-20 lg:px-40 xl:px-60">
@@ -10,24 +33,25 @@
     <h2 class="text-xl font-bold mb-1 text-white">Ayo Kita Masak Masakan Yang Mantap</h2>
   </div>
 </section>
+
 <section class="bg-gray-600">
-<div class="text-white px-8 sm:px-12 md:px-24 lg:px-40 xl:px-60 py-10">
-  <div class="flex flex-col md:flex-row justify-between">
-    <div class="w-full md:w-1/2 mb-8 md:mb-0">
-      <h3 class="text-2xl font-bold mb-4">Bahan-bahan:</h3>
-      <ul class="list-disc pl-6">
-        <li class="mb-2">Nasi putih (1 mangkuk)</li>
-        <li class="mb-2">Telur (2 butir, kocok lepas)</li>
-        <li class="mb-2">Bawang putih (2 siung, cincang halus)</li>
-        <li class="mb-2">Bawang merah (2 siung, cincang halus)</li>
-        <li class="mb-2">Cabe merah (2 buah, iris tipis)</li>
-        <li class="mb-2">Wortel (1 buah, iris tipis)</li>
-        <li class="mb-2">Kol (1/4 buah, iris tipis)</li>
-        <li class="mb-2">Saus tiram (1 sdm)</li>
-        <li class="mb-2">Kecap manis (2 sdm)</li>
-        <li>Garam (secukupnya)</li>
-      </ul>
-    </div>
+  <div class="text-white px-8 sm:px-12 md:px-24 lg:px-40 xl:px-60 py-10">
+    <div class="flex flex-col md:flex-row justify-between">
+      <div class="w-full md:w-1/2 mb-8 md:mb-0">
+        <h3 class="text-2xl font-bold mb-4">Bahan-bahan:</h3>
+          <ul class="list-disc pl-6">
+            <li class="mb-2">Nasi putih (1 mangkuk)</li>
+            <li class="mb-2">Telur (2 butir, kocok lepas)</li>
+            <li class="mb-2">Bawang putih (2 siung, cincang halus)</li>
+            <li class="mb-2">Bawang merah (2 siung, cincang halus)</li>
+            <li class="mb-2">Cabe merah (2 buah, iris tipis)</li>
+            <li class="mb-2">Wortel (1 buah, iris tipis)</li>
+            <li class="mb-2">Kol (1/4 buah, iris tipis)</li>
+            <li class="mb-2">Saus tiram (1 sdm)</li>
+            <li class="mb-2">Kecap manis (2 sdm)</li>
+            <li>Garam (secukupnya)</li>
+          </ul>
+      </div>
     <div class="w-full md:w-1/2">
       <h3 class="text-2xl font-bold mb-4">Cara Memasak:</h3>
       <ol class="list-decimal pl-6">
@@ -43,6 +67,7 @@
   </div>
 </div>
 </section>
+
    <section class="bg-gray-100" style="background-image: url('https://cdn.idntimes.com/content-images/community/2021/11/amanda-lim-2-vyvemgd04-unsplash-imresizer-5f33a6106b7b2abdfec00ff311918826-a32e6a0184b0e1b99c076973bfbeb5cf.jpg'); background-size: cover; background-position: center;">
       <div class="px-4 sm:px-10 md:px-20 lg:px-40 xl:px-60 py-6">
         <h2 class="text-2xl font-bold mb-4">Video Tutorial:</h2>
@@ -52,6 +77,7 @@
       </div>
     </section>
   </main>
+
   <footer class="bg-gray-900 py-2 text-white text-center">
     <p>© 2023, Tutorial Masak Nasi Goreng Adit</p>
   </footer>
